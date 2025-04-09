061712023c68e314c06df1b1b25b12ea0ffe503c
whar0012 <whar0012@algonquinlive.com>
2025-04-09 16:40:03 -0400

    complete (new)

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "mtm6404-suess-treasury"
+  base: "mtm6404-suess-treasury",
   plugins: [react()]
 })
