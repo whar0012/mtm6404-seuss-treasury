b3092111a4266bf8535f5801ab09dff241a2dc97
whar0012 <whar0012@algonquinlive.com>
2025-04-09 17:12:26 -0400

    Finished

--- src/router.jsx
+++ src/router.jsx
@@ -4,27 +4,32 @@ import { QuotesPage } from './pages/quotes-page'
 import { BooksDetailPage } from './pages/books-detail-page'
 import { Layout } from './components/layout'
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <BooksPage />,
+        },
+        {
+          path: '/books',
+          element: <BooksPage />,
+        },
+        {
+          path: 'books/:id',
+          element: <BooksDetailPage />,
+        },
+        {
+          path: '/quotes',
+          element: <QuotesPage />,
+        },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: '/',
-        element: <BooksPage />,
-      },
-      {
-        path: '/books',
-        element: <BooksPage />,
-      },
-      {
-        path: 'books/:id',
-        element: <BooksDetailPage />,
-      },
-      {
-        path: '/quotes',
-        element: <QuotesPage />,
-      },
-    ],
+    basename: import.meta.env.VITE_BASE_NAME || '/',
   },
-])
+)
